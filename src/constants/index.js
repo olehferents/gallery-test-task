1cfb6941824fe8a9d9d0a343097b45f7ccb322eb
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-22 20:03:23 +0200

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const FETCH_PHOTOS_SUCCEED = 'FETCH_PHOTOS_SUCCEED';
+
+export const FETCH_PHOTOS_FAILED = 'FETCH_PHOTOS_FAILED';
+
+export const API_URL = 'https://api.unsplash.com/';
+
+export const CLIENT_ID = '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043';
+
+export const PER_PAGE = "30";
