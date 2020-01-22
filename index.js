e39e41bba8a1309e9e6fea6cc103d06b08c8413b
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-22 20:08:06 +0200

    Add redux provider

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import React from "react";
+import { Provider } from 'react-redux';
 import App from "./src/App";
+import store from './src/store/index';
 
-AppRegistry.registerComponent(appName, () => App);
+const Redux = () => (
+    <Provider store={ store }>
+        <App/>
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => Redux);
