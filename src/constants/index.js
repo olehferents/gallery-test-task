be918c359e64c51dda253c4fad27f36d50fee70b
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-23 17:36:51 +0200

    Remove PER_PAGE constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,5 +5,3 @@ export const FETCH_PHOTOS_FAILED = 'FETCH_PHOTOS_FAILED';
 export const API_URL = 'https://api.unsplash.com/';
 
 export const CLIENT_ID = '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043';
-
-export const PER_PAGE = "30";
