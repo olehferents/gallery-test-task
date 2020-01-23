6d439bb688c65ea3c35cb576b124a229bc3f1bf4
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-23 22:00:15 +0200

    Add react-navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
-import Header from "./components/Header";
-import PhotoListContainer from "./containers/PhotoListContainer";
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from "react-navigation-stack";
 
-const App = () => {
-    return (
-        <View>
-            <Header/>
-            <PhotoListContainer/>
-        </View>
-    );
-};
+import FullImageScreen from "./screens/FullImageScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const AppNavigator = createStackNavigator({
+        Home: {screen: HomeScreen},
+        FullImage: {screen: FullImageScreen},
+    },
+    {
+        initialRouteName: 'Home'
+    }
+);
+
+const App = createAppContainer(AppNavigator);
 
 export default App;
